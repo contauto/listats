07579649293e8e8038c6365b328d8e8a9dc02827
Kağan Berke Maktav <controleng@yandex.com>
2023-03-22 16:32:27 +0300

    dropdown show button disable

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useEffect, useRef, useState } from "react";
 import { Navbar, Button, Link, Text, useTheme } from "@nextui-org/react";
 import {
   ARTIST_LONG_TERM,
@@ -67,6 +67,41 @@ export default function Nav() {
   };
 
   const pendingApiCall = useApiProgress("get", base, false);
+  const trackRef = useRef(null);
+  const artistRef = useRef(null);
+  const [trackDropdownVisible, setTrackDropdownVisible] = useState(false);
+  const [artistDropdownVisible, setArtistDropdownVisible] = useState(false);
+  const [howMany, setHowMany] = useState(0);
+
+  useEffect(() => {
+    const dropdownClickTracker = (event) => {
+      if (
+        ((artistRef.current === null ||
+        !artistRef.current.contains(event.target)) &&
+        (trackRef.current === null ||
+        !trackRef.current.contains(event.target))) ||
+        howMany > 1
+      ) {
+        setHowMany(0);
+        setTrackDropdownVisible(false);
+        setArtistDropdownVisible(false);
+      }
+    };
+    document.addEventListener("click", dropdownClickTracker);
+    return () => {
+      document.removeEventListener("click", dropdownClickTracker);
+    };
+  }, [isLoggedIn, howMany]);
+
+  let trackDropDownClass = "dropdown-menu ";
+  let artistDropDownClass = "dropdown-menu ";
+
+  if (trackDropdownVisible) {
+    trackDropDownClass += "show";
+  }
+  if (artistDropdownVisible) {
+    artistDropDownClass += "show";
+  }
 
   return (
     <Navbar isBordered={isDark} variant="sticky">
@@ -85,13 +120,18 @@ export default function Nav() {
           <Navbar.Item>
             <div className="dropdown">
               <button
+                id="dropdown"
                 className="btn btn-secondary dropdown-toggle"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+                disabled={pendingApiCall}
+                onClick={() => {
+                  setTrackDropdownVisible(true);
+                  setHowMany(howMany + 1);
+                }}
+                ref={trackRef}
               >
                 Track
               </button>
-              <ul className="dropdown-menu">
+              <ul className={trackDropDownClass}>
                 <li>
                   <h6
                     style={{ cursor: "pointer" }}
@@ -145,13 +185,18 @@ export default function Nav() {
           <Navbar.Item>
             <div className="dropdown">
               <button
+                id="dropdown"
+                disabled={pendingApiCall}
                 className="btn btn-secondary dropdown-toggle"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+                ref={artistRef}
+                onClick={() => {
+                  setArtistDropdownVisible(true);
+                  setHowMany(howMany + 1);
+                }}
               >
                 Artist
               </button>
-              <ul className="dropdown-menu">
+              <ul className={artistDropDownClass}>
                 <li>
                   <h6
                     style={{ cursor: "pointer" }}
